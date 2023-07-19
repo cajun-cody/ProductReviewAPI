e2f22c737c7e2611fedfb1d382c37ab69685d8b1
Cody <cemt5462@gmail.com>
2023-07-19 15:03:30 -0500

    Created DTO files

---
+++ ProductsReviewWebAPI/DataTransferObjects/ProductDTO.cs
@@ -0,0 +1,12 @@
+﻿using ProductsReviewWebAPI.Models;
+
+namespace ProductsReviewWebAPI.DataTransferObjects
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ ProductsReviewWebAPI/DataTransferObjects/ReviewDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductsReviewWebAPI.DataTransferObjects
+{
+    public class ReviewDTO
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int Rating { get; set; }
+    }
+}
